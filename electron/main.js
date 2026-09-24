b4f12d6930b9a17cd9cc658d575d41927f05aeb2
Srikanth.CH <srikanth11316@gmail.com>
2026-09-24 16:03:06 +0530

    Add Electron desktop app for Windows EXE (wraps Cloudflare Pages frontend)

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { autoUpdater } = require('electron-updater');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadURL('https://xenowebsite.pages.dev/chat');
+
+  mainWindow.on('closed', () => {
+    app.quit();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
